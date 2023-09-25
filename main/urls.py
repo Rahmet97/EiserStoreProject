758320fb425112b80090befbd06480e7159ea30e
Isomiddin21 <isomiddinwtu@gmail.com>
2023-09-25 10:52:35 +0500

    base.html

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.urls import path
 
-from .views import CategoryView
+from .views import CategoryView, home
 
 urlpatterns = [
-    path('category', CategoryView.as_view, name='category')
+    path('category', CategoryView.as_view, name='category'),
+    path('', home())
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views import View
+
+# Create your views here.
 
 
-class CategoryView(View):
-    def get(self, request):
-        return render(request, 'category.html')
+def home(request):
+    return render(request, 'index.html')
+
+def get(self, request):
+    return render(request, 'category.html')
