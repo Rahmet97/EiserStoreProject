06a1f5cc0d520f7d6a7a1e68f6d45ea98ef76893
ahmatovdilshodbek <ahmatovdilshodbek@gmail.com>
2023-09-25 10:51:40 +0500

    add category

--- main/models.py
+++ main/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+
+class CategoryProduct(models.Model):
+    name = models.CharField(max_length=150)
+    images = models.ImageField(upload_to=True)
+    price = models.IntegerField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
 class User(models.Model):
     sdm = models.CharField(max_length=255),
     dms = models.CharField(max_length=255),
