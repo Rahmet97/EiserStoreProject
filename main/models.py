2da9895a51f1617bff7c9dcd60ec4d7ed81c198d
Isomiddin21 <isomiddinwtu@gmail.com>
2023-09-25 11:22:04 +0500

    base.html

--- main/models.py
+++ main/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class User(models.Model):
+    sdm = models.CharField(max_length=255),
+    dms = models.CharField(max_length=255),
+
+
+# Create your models here.
 
 class CategoryProduct(models.Model):
     name = models.CharField(max_length=150)
